94a563e2d285484f91e17326c6b8155f6aa3e02f
Fiki <martin.fikejz@gmail.com>
2022-04-14 09:47:43 +0200

    Systemd service added

--- app/app.py
+++ app/app.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python3
 
-import os, psutil
-
 from flask import Flask
 from prometheus_client import Counter, Gauge, generate_latest
 
 app = Flask(__name__)
 
 c = Counter('visit_counter','Page visit counter')
-
-def get_process_metrics():
-    proc = psutil.Process(os.getpid())
-    return proc.memory_info().rss
-
+count = 0
 
 @app.route("/")
 def message():
+    global count
     c.inc()
-    vv = get_process_metrics()
-    return "Hello world! " + str(vv)
+    count += 1
+    return "This page has been visited " + str(count) + " times!"
 
 
 @app.route("/metrics")
 def metrics():
     return generate_latest()
-#return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == '__main__':
